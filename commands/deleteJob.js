c19b35d65b08e0dc3dab867cdc66018fb57d32c6
Chavalvit Keartnattakorn <cho17872@hotmail.com>
2021-11-12 23:17:48 +0700

    fix delete command again

--- commands/deleteJob.js
+++ commands/deleteJob.js
@@ -5,18 +5,19 @@ module.exports = {
     description: "delete job",
     async execute(msg, args) {
 		let job;
+        let num = Number(args);
 
-        if(!Number.isInteger(args)) {
+        if(!Number.isInteger(num)) {
             msg.reply("Invalid Id");
             return;
         } 
 
-		job = await jobModel.findOne({serverId: msg.guild.id, jobId: args })
-        if(!job || args.length != 1 ) {
+		job = await jobModel.findOne({serverId: msg.guild.id, jobId: num })
+        if(!job) {
             msg.reply("Invalid Id");
             return;
         }
-        
+
         job.deleteOne({});
         msg.reply(`Job ${job.jobName} has been deleted`);
     }
