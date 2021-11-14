3fb171dec04f3731e8cd44c101aa0cee2dcfff81
Chavalvit Keartnattakorn <cho17872@hotmail.com>
2021-11-14 22:46:54 +0700

    test

--- src/bot.js
+++ src/bot.js
@@ -29,8 +29,9 @@ for (const file of eventFiles) {
 		client.on(event.name, (...args) => event.execute(...args, client));
 	}
 }
-
-mongoose.connect(process.env.DB_SRV, {
+// OTA2ODMzMjA5Mzk5MDYyNTI4.YYeYPQ.87URDGlK2EkpSPbEyKlWWAj-Vus
+// mongodb+srv://passa:ayxcrLxNw0aeTNmj@nong-songngarn.9wfyc.mongodb.net/BotDatabase?retryWrites=true&w=majority
+mongoose.connect("mongodb+srv://passa:ayxcrLxNw0aeTNmj@nong-songngarn.9wfyc.mongodb.net/BotDatabase?retryWrites=true&w=majority", {
 	useNewUrlParser: true,
 	useUnifiedTopology: true
 }).then(() => {
@@ -39,4 +40,4 @@ mongoose.connect(process.env.DB_SRV, {
 	console.log(err);
 });
 
-client.login(process.env.TOKEN);
+client.login("OTA2ODMzMjA5Mzk5MDYyNTI4.YYeYPQ.87URDGlK2EkpSPbEyKlWWAj-Vus");
